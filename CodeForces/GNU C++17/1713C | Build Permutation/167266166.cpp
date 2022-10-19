9241a206dfe2e215474176b4a1b0fedb68155cea
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:14:57 -0700

    Committing files

---
+++ CodeForces/GNU C++17/1713C | Build Permutation/167266166.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+template<class T> using V = vector<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+template<class T> bool smin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool smax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+ 
+const int MOD = 1e9+7; // 998244353
+const int MX = 2e5+5;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+ 
+void solve() {
+    int n;
+    cin >> n;
+    int on = n;
+    V<int> ans(n);
+    while (n) {
+        int x = 0;
+        while (x*x < n-1) x++;
+        int c = x*x;
+        for (int i = c-(n-1); i < n; i++) {
+            ans[i] = c-i;
+        }
+        n = c-(n-1);
+    }
+    for (int i = 0; i < on; i++) cout << ans[i] << " ";
+    cout << "\n";
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
