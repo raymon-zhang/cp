2e0456bfdce875814c689ad410e1a41214b1d316
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:15:25 -0700

    Committing files

---
+++ CodeForces/GNU C++17/1716A | 2-3 Moves/166932680.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+template<class T> using V = vector<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+template<class T> bool smin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool smax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+ 
+const int MOD = 1e9+7; // 998244353
+const int MX = 2e5+5;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        if (n == 1) cout << "2\n";
+        else {
+            int count = 0;
+            while (n%3) {
+                count++;
+                n -= 2;
+            }
+            count += n/3;
+            cout << count << "\n";
+        }
+    }
+}
