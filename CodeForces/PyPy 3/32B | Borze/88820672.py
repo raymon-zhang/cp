92185e256334e4248f69d980b608efbb4b83eecf
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:26:05 -0700

    Committing files

---
+++ CodeForces/PyPy 3/32B | Borze/88820672.py
@@ -0,0 +1,24 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s)]))
+def invr():
+    return(map(int,input().split()))
+ 
+s = input()
+ 
+m = { '-.': "1", '--':"2"}
+ 
+i = 0
+out = ""
+while i < len(s):
+    if s[i] == ".":
+        out += "0"
+    elif s[i] == "-":
+        out += m[s[i:i+2]]
+        i += 1
+    i += 1
+print(out)
