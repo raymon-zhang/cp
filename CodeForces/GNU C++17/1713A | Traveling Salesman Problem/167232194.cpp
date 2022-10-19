f82e2a00f96c8d749eb3918089fd133832beba0c
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:15:03 -0700

    Committing files

---
+++ CodeForces/GNU C++17/1713A | Traveling Salesman Problem/167232194.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+template<class T> using V = vector<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+template<class T> bool smin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool smax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+ 
+const int MOD = 1e9+7; // 998244353
+const int MX = 2e5+5;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+ 
+void solve() {
+    int n;
+    cin >> n;
+    int px = 0, nx = 0, py = 0, ny = 0;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        if (x == 0) {
+            py = max(py, y);
+            ny = min(ny, y);
+        }
+        else {
+            px = max(px, x);
+            nx = min(nx, x);
+        }
+    }
+    cout << 2*px - 2*nx + 2 * py - 2*ny << "\n";
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
