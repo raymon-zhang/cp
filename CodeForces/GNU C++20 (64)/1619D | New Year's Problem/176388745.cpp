a5f0d8e96ea89223f7ca66d6e1ebacd296f21e28
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:11:30 -0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1619D | New Year's Problem/176388745.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+template<class T> using V = vector<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+template<class T> bool smin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool smax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+ 
+const int MOD = 1e9+7; // 998244353
+const int MX = 2e5+5;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+ 
+void solve() {
+    int m, n;
+    cin >> m >> n;
+ 
+    V<V<int>> p(m, V<int>(n));
+ 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> p[i][j];
+        }
+    }
+ 
+    int l = 0, r = 1e9;
+    while (l < r) {
+        int mid = (l+r+1)/2;
+        V<bool> good(n);
+        bool dup = false;
+        for (int i = 0; i < m; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (p[i][j] >= mid) {
+                    count++;
+                    good[j] = true;
+                }
+            }
+            if (count >= 2) dup = true;
+        }
+        
+        bool covered = true;
+        for (int j = 0; j < n; j++) {
+            if (!good[j]) covered = false;
+        }
+ 
+        if ((covered) && dup) {
+            l = mid;
+        } else {
+            r = mid-1;
+        }
+    }
+    cout << l << "\n";
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
