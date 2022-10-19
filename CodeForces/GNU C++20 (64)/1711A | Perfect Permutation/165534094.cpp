d9ecf1b768231f7f670c4da72ae049f8a8469a84
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:16:24 -0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1711A | Perfect Permutation/165534094.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        for (int i = 2; i <= n; i++) {
+            cout << i << " ";
+        }
+        cout << 1 << "\n";
+    }
+}
