05d1a71ea317b42605f027bae737196ae21df105
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:23:33 -0700

    Committing files

---
+++ CodeForces/PyPy 3/1469A | Regular Bracket Sequence/102573459.py
@@ -0,0 +1,58 @@
+import sys
+input = sys.stdin.readline
+ 
+ 
+def inInt():
+    return int(input())
+ 
+ 
+def inStr():
+    return input().strip("\n")
+ 
+ 
+def inIList():
+    return(list(map(int, input().split())))
+ 
+ 
+def inSList():
+    return(input().split())
+ 
+ 
+def bsearch(nums, target):
+    N = len(nums or [])
+    l = 0
+    r = N - 1
+ 
+    while l <= r:
+        mid = ((l + r) // 2)
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
+        else:
+            return (None, mid, None)
+ 
+    return (r if r >= 0 else None, None, l if l <= N-1 else None)
+ 
+ 
+def yesOrNo(val):
+    print("YES" if val else "NO")
+ 
+ 
+def printSpacedArray(nums):
+    print(*nums)
+ 
+ 
+def solve():
+    s = inStr()
+ 
+    if s[0] != ")" and s[-1] != "(":
+        c = s.count("?")
+        if (c % 2) == 0:
+            return "YES"
+    return "NO"
+ 
+ 
+tests = inInt()
+for case in range(tests):
+    print(solve())
