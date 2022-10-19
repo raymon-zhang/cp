da8fd5e770cb0a2fc5dc19b2fc5501dec61ce2bf
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-10-18 21:15:21 -0700

    Committing files

---
+++ CodeForces/GNU C++17/1716B | Permutation Chain/166945842.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define pb push_back
+#define F first
+#define S second
+ 
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+ 
+template<class T> using V = vector<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+template<class T> bool smin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool smax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+ 
+const int MOD = 1e9+7; // 998244353
+const int MX = 2e5+5;
+const int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 1; i <= n; i++) {
+        nums.pb(i);
+    }
+    cout << n << "\n";
+    for (int j: nums) cout << j << " ";
+    cout << "\n";
+    for (int i = 0; i < n-1; i++) {
+        swap(nums[i], nums[i+1]);
+        for (int j: nums) cout << j << " ";
+        cout << "\n";
+    }
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
